d31601150a5cf9efefdcd18bc4e549fc12d09d0a
plaiaundi <plaiaundi@MININT-5OLIS4G>
2024-02-07 14:28:07 +0100

    Conectar el conector

--- src/Modelo/Conector.java
+++ src/Modelo/Conector.java
@@ -1,5 +1,43 @@
 package Modelo;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Conector {
 
+	protected Connection cn;
+	 
+	public void conectar() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			String url = "jdbc:mysql://localhost/biblioteca";
+			cn = (Connection) DriverManager.getConnection(url,"root","");
+			
+		} catch (ClassNotFoundException e) {
+			System.out.println("Error al importar Driver");
+			e.printStackTrace();
+		} catch (SQLException e) {
+			System.out.println("Error conexion");
+			e.printStackTrace();
+		}
+	}
+	
+	public void cerrar() {
+		try {
+			cn.close();
+		} catch (SQLException e) {
+			System.out.println("Error al cerrar la conexion");
+			e.printStackTrace();
+		}
+	}
+
+	public Connection getCn() {
+		return cn;
+	}
+
+	public void setCn(Connection cn) {
+		this.cn = cn;
+	}
+	
 }
--- src/Modelo/GestorBBDD.java
+++ src/Modelo/GestorBBDD.java
@@ -1,5 +1,11 @@
 package Modelo;
 
-public class GestorBBDD {
+public class GestorBBDD extends Conector {
 
+	public void insertarLibro(Libro libro) {
+		
+	}
+	public void eliminarLibro(int id) {
+		
+	}
 }
