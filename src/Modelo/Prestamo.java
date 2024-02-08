1fd8780dac74d6865681b11af2c5912d95d40d0b
plaiaundi <plaiaundi@MININT-5OLIS4G>
2024-02-08 09:58:58 +0100

    Clase prestamo

--- src/Modelo/Prestamo.java
+++ src/Modelo/Prestamo.java
@@ -1,5 +1,40 @@
 package Modelo;
 
+import java.util.Date;
+
 public class Prestamo {
 
+	//Atributos
+		private Libro libro;
+		private Socio socio;
+		private Date fecha;
+		private boolean devuelto;
+	
+	//Getter&Setter
+		public Libro getLibro() {
+			return libro;
+		}
+		public void setLibro(Libro libro) {
+			this.libro = libro;
+		}
+		public Socio getSocio() {
+			return socio;
+		}
+		public void setSocio(Socio socio) {
+			this.socio = socio;
+		}
+		public Date getFecha() {
+			return fecha;
+		}
+		public void setFecha(Date fecha) {
+			this.fecha = fecha;
+		}
+		public boolean isDevuelto() {
+			return devuelto;
+		}
+		public void setDevuelto(boolean devuelto) {
+			this.devuelto = devuelto;
+		}
+	
+	
 }
